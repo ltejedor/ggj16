b15f6f857f29e8e82cd7048bc0e0507ebfb421ff
Leandra Tejedor <babe@Leandras-MacBook.local>
2016-01-30 23:57:29 -0500

    finished twine2json v1!

--- textProcessing/twine2json.py
+++ textProcessing/twine2json.py
@@ -1,51 +1,76 @@
 import json
 
+# conversation = [p,q,p,q,...]
+# p = [prompt,prompt,prompt,prompt]
+# prompts are dictionaries with
+# .prompt_txt
+# .response_txt
+# .trust_pts 
+# .love_pts 
+# q = [question_string,[answer,answer,answer,answer]]
+# answers are dictionaries with 
+# .answer_txt
+# .trust_pts 
+# .love_pts 
+
 def inputOps(filename):
-	convDict = dict()
-	inQ     = False
-	inP     = False
-	i       = 0 # turn number
+	conversation = []
+	inQ          = False
+	inP          = False
 	with open(filename, 'r') as f:
 		for uglyline in f:
 			line = uglyline.replace("\n","")
 			if line.startswith("TURN"):
+				if inP:
+					conversation.append(p)
+				if inQ:
+					conversation.append(q)
 				inQ = False
 				inP = False
-				i   = int("".join([x for x in line if x.isdigit()]))
 			elif line.capitalize().startswith("Question: "):
-				inQ = True
+				inQ   = True
 				qtext = line.replace("Question: ","")
-				convDict[i] = dict{}
-				convDict[i]["type"]="question"
-				convDict[i]["qtext"] = qtext
-				convDict[i]["answers"]= dict()
+				q     = [qtext,[]]
 			elif line.startswith("*"):
 				if inQ:
-					ans = line.replace("*","")
-					scoreStart = ans.index("[")
-					scoreEnd = ans.index("]")
-					numString = ans[scoreStart+1:scoreEnd].strip()
-					ansText = ans.replace(ans[scoreStart-1:scoreEnd+1],"")
-					ansDict = dict()
-					ansDict["text"] = ansText
-					ansDict["pts"]  = int(numString)
-					print(i)
-					print(json.dumps(convDict))
-					convDict[i].["answers"]["text"]
-					
+					ans          = line.replace("*","")
+					loveStart    = ans.index("[")
+					loveEnd      = ans.index("]")
+					love_string  = ans[loveStart+1:loveEnd].strip()
+					ans          = ans.replace(ans[loveStart-1:loveEnd+1],"")
+					trustStart   = ans.index("<")
+					trustEnd     = ans.index(">")
+					trust_string = ans[trustStart+1:trustEnd].strip()	
+					ansText      = ans.replace(ans[trustStart-1:trustEnd+1],"")
+					ansDict      = dict()
+					ansDict["answer_text"] = ansText
+					ansDict["trust_pts"]   = int(trust_string)
+					ansDict["love_pts"]    = int(love_string)
+					q[1].append(ansDict)
+
 				else:
-					prompt = line.replace("*","")
-					scoreStart = prompt.index("[")
-					scoreEnd = prompt.index("]")
-					numString = prompt[scoreStart+1:scoreEnd].strip()
-					promptText = prompt.replace(prompt[scoreStart-1:scoreEnd+1],"")
 					if inP == False:
-						pDict = dict()
-						inP   = True
-					pDict["text"] = promptText
-					pDict["pts"]  = int(numString)
-					convDict[i].append(pDict)
-	return convDict
+						inP = True
+						p = []
+					prompt       = line.replace("*","")
+					loveStart    = prompt.index("[")
+					loveEnd      = prompt.index("]")
+					love_string  = prompt[loveStart+1:loveEnd].strip()
+					prompt       = prompt.replace(prompt[loveStart-1:loveEnd+1],"")
+					trustStart   = prompt.index("<")
+					trustEnd     = prompt.index(">")
+					trust_string = prompt[trustStart+1:trustEnd].strip()	
+					prompt_string        = prompt.replace(prompt[trustStart-1:trustEnd+1],"")
+					pDict                = dict()
+					pDict["prompt_text"] = prompt_string
+					pDict["love_pts"]    = int(love_string)
+					pDict["trust_pts"]   = int(trust_string)
+			elif line.startswith("#"):
+					response              = line.replace("#","")
+					pDict["response_txt"] = response
+					p.append(pDict)
+
+	return conversation
 
 
 def outputOps(obj,outname="out.json"):
@@ -53,9 +78,11 @@ def outputOps(obj,outname="out.json"):
 		f.write(json.dumps(obj,indent=2))
 
 def twine2json(filename,outname):
-	convDict = inputOps(filename)
-	print(json.dumps(convDict))
-	outputOps(convDict)
+	conversation = inputOps(filename)
+	print(json.dumps(conversation))
+	outputOps(conversation)
 
 if __name__ == "__main__":
 	twine2json("sample.txt","outfile.txt")
+
+	
