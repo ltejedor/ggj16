74f15e0a0d005023b3b5260e3bb3d8c35a4fb479
Leandra Tejedor <lmtejedor@gmail.com>
2016-01-31 13:59:09 -0500

    add first dating profile screen

--- js/main.js
+++ js/main.js
@@ -1,1 +1,40 @@
+dateStart();
+var datingSiteVisible;
+var dateTurns = 0;
+var convoTurns = 0;
 
+function dateStart(){
+	datingSite();
+}
+
+function datingSite(){
+	$( ".js-cover" ).fadeIn( "slow" );
+	$('.js-dating-profile').slideDown("slow");
+	datingSiteVisible = true;
+}
+
+if(datingSiteVisible){
+	$('.js-cover, .js-start-btn, .js-x-btn').click(function(){
+		$( ".js-cover" ).fadeOut( "slow" );
+		$('.js-dating-profile').slideUp("slow");
+		datingSiteVisible = false;
+	});
+	startConversation();
+}
+
+function startConversation(){
+	for(var i = 0; i < conversation[dateTurns].length; i++){
+		var currentTopic = conversation[dateTurns][i].prompt_text;
+		$('.js-topic-' + i).text(currentTopic);
+	}
+}
+
+$('.js-topic-btn').click(function(){
+	var thisTopicNum = parseInt($(this).attr('name'));
+	console.log(thisTopicNum)
+	$('.js-they-textbox').fadeIn();
+	$('.js-they-textbox-content').text(conversation[dateTurns][thisTopicNum].response_txt);
+
+
+	dateTurns++;
+});
